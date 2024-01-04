f5b6b02283cf0a16fbff1d398e41e89ffbc2df05
Tobias <tobias.maier@st.oth-regensburg.de>
2024-01-04 13:39:46 +0100

    working sort for Name, Kindergarten, Anmeldedatum

--- src/app/dashboard/data/data.component.ts
+++ src/app/dashboard/data/data.component.ts
@@ -5,6 +5,10 @@ import { StoreService } from 'src/app/shared/store.service';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { CommonModule } from '@angular/common';
 import { PageEvent } from '@angular/material/paginator';
+import { MatSortModule, MatSort, Sort } from '@angular/material/sort';
+import { ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Child } from 'src/app/shared/interfaces/Child';
 
 @Component({
   selector: 'app-data',
@@ -13,6 +17,7 @@ import { PageEvent } from '@angular/material/paginator';
   standalone: true,
   imports: [
     MatPaginatorModule,
+    MatSortModule,
     CommonModule
   ]
 })
@@ -21,10 +26,45 @@ export class DataComponent implements OnInit {
   constructor(public storeService: StoreService, private backendService: BackendService) {}
   @Input() currentPage!: number;
   @Output() selectPageEvent = new EventEmitter<number>();
+  // @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatSort, { static: true }) sort!: MatSort;
   public page: number = 0;
+  public dataSource!: MatTableDataSource<Child>;
+  ;
 
   ngOnInit(): void {
+      // this.backendService.getChildren(this.currentPage).subscribe(data => {
+      //   this.dataSource.data = data;
+      //   this.dataSource.sort = this.sort;
+      // });
     this.backendService.getChildren(this.currentPage);
+    // this.backendService.getChildren(this.currentPage).subscribe(data => {
+    //   this.dataSource.data = data;
+    // });
+    // // this.storeService.children.sort = this.sort;
+    // this.dataSource = new MatTableDataSource(this.storeService.children);
+    // this.dataSource.sort = this.sort;
+    
+  }
+
+  sortChildrenBy(criteria: string): void {
+    this.storeService.children.sort((a: any, b: any) => {
+      if (criteria === 'kindergarden.name') {
+        return a.kindergarden.name.localeCompare(b.kindergarden.name);
+      } else {
+        const aValue = a[criteria];
+        const bValue = b[criteria];
+  
+        // Assuming the values are strings for simplicity
+        if (aValue < bValue) {
+          return -1;
+        }
+        if (aValue > bValue) {
+          return 1;
+        }
+        return 0;
+      }
+    });
   }
 
   getAge(birthDate: string) {
