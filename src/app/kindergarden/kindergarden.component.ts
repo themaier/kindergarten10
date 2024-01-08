d115c6a0dc6d518762f68d947babf534f4d85b6e
Tobias <tobias.maier@st.oth-regensburg.de>
2024-01-08 10:48:37 +0100

    selecting Kindergarten for Infos working

--- src/app/kindergarden/kindergarden.component.ts
+++ src/app/kindergarden/kindergarden.component.ts
@@ -3,8 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { BackendService } from 'src/app/shared/backend.service';
 import { StoreService } from 'src/app/shared/store.service';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators'
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-kindergarden',
@@ -12,6 +11,7 @@ import { map } from 'rxjs/operators'
   styleUrls: ['./kindergarden.component.scss'],
   standalone: true,
   imports: [
+    ReactiveFormsModule,
     CommonModule
   ],
 })
@@ -28,57 +28,23 @@ export class KindergardenPageComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    // this.allKindergardens=this.backendService.getKindergardens();
-    // this.selectedKindergarten=this.backendService.getKindergartenDetails(1);
-    // console.log(this.allKindergardens.find((kg: any) => kg.id === 1))
-    // console.log("Hallo")
-    // this.selectedKindergarten=1
-    // console.log(this.selectedKindergarten)
     const kindergardenIdControl = this.form.get('kindergardenId');
-    this.backendService.getKindergartenDetails(1).subscribe(
-      data => {
-        if (data) {
-          // Handle the received details
-          console.log(data);
-          this.selectedKindergarten=data;
-        }
-      }
-    );
-    // this.form.get('kindergardenId').valueChanges.subscribe(selectedId => {
-    //   // Handle the selected ID
-    //   console.log('Selected Kindergarten ID:', selectedId);
-    //   // You can call a function here to fetch additional details based on selectedId
-    // });
-    // if (kindergardenIdControl) {
-    //   kindergardenIdControl.valueChanges.subscribe(kindergardenId => {
-    //     if (kindergardenId) {
-    //       this.selectedKindergarten = this.backendService.getKindergartenDetails(kindergardenId);
-    //     } else {
-    //       this.selectedKindergarten = null;
-    //     }
-    //   });
-    // }
 
     if (kindergardenIdControl) {
       kindergardenIdControl.valueChanges.subscribe(kindergardenId => {
         if (kindergardenId) {
-          // this.selectedKindergarten = this.backendService.getKindergartenDetails(kindergardenId);
-          this.backendService.getKindergartenDetails(1).subscribe(
-            data => {
-              if (data) {
-                // Handle the received details
-                console.log(data);
-                this.selectedKindergarten=data;
-              }
-            }
-          );
-        } else {
-          this.selectedKindergarten = null;
-        }
-      });
+          console.log("Selection triggered!", kindergardenId)
+          this.backendService.getKindergartenDetails(Number(kindergardenId)).subscribe(
+              data => {
+                if (data) {
+                  // Handle the received details
+                  console.log(data);
+                  this.selectedKindergarten=data;
+                } else {
+                  this.selectedKindergarten=null;
+              }}
+            );
+      }})
     }
-  }
-
-  onSubmit() {
   }
 }
